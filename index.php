7ae5dd6d6f84c0a1047bb6c6fceb69904e483f6a
jacobking <billbook@163.com>
2015-02-27 12:25:29 +0800

    11

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Start</title>
+</head>
+<body>
+
+<?php
+	phpinfo();
+?>
+</body>
+</html>
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
-book mark
+Git is a distributed version control system.
+Git is free software distributed under the GPL.
+showtime
